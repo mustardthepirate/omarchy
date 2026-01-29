9969b358ad5cdad4ba3b5ad8f106081e85f9b63e
Shane Adams <shanejadams@gmail.com>
2026-01-29 13:48:16 +0000

    Fix limine.conf migration to preserve symlink; make refresh-limine safer

--- migrations/1762446739.sh
+++ migrations/1762446739.sh
@@ -9,5 +9,9 @@ if omarchy-cmd-present limine; then
 
   sudo rm -f /boot/EFI/limine/limine.conf
   sudo rm -f /boot/EFI/BOOT/limine.conf
-  sudo rm -f /boot/limine/limine.conf
+
+  # Only remove /boot/limine/limine.conf if it's a real file (not our symlink)
+  if [ -e /boot/limine/limine.conf ] && [ ! -L /boot/limine/limine.conf ]; then
+    sudo rm -f /boot/limine/limine.conf
+  fi
 fi
